0d3aec43f6a41c6d72fe12f2ff35be38b99ac9f1
Wood-Benjamin <38670310+Wood-Benjamin@users.noreply.github.com>
2019-11-10 01:26:34 -0700

    Update Myhttpurlconnection.java

--- HTTPURLConnection/CIT360_HttpURLConnection/src/myhttpurlconnection/Myhttpurlconnection.java
+++ HTTPURLConnection/CIT360_HttpURLConnection/src/myhttpurlconnection/Myhttpurlconnection.java
@@ -1,7 +1,6 @@
 package myhttpurlconnection;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -11,7 +10,7 @@ public class Myhttpurlconnection {
 
     private static Scanner in;
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) {
         in = new Scanner(System.in);
         String firstname = null;
         String lastname = null;
@@ -22,18 +21,25 @@ public class Myhttpurlconnection {
         lastname = in.next();
 
         String myphplink = "http://localhost:10080/java/myhttpurlconnect.php?firstname="+firstname+"&lastname="+lastname;
-        URL url = new URL(myphplink);
-        HttpURLConnection connect = (HttpURLConnection)url.openConnection();
-        connect.setRequestMethod("GET");
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
-        StringBuilder sb = new StringBuilder();
-        String line;
+        try {
+            URL url = new URL(myphplink);
+            HttpURLConnection connect = (HttpURLConnection) url.openConnection();
+            connect.setRequestMethod("GET");
 
-        while((line = in.readLine()) != null){
-            sb.append(line);
+            BufferedReader in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line;
+
+            while ((line = in.readLine()) != null) {
+                sb.append(line);
+            }
+            in.close();
+            System.out.println(sb.toString());
+        } catch (Exception e) {
+            System.out.println("Sorry, something went wrong.");
+        } finally {
+            System.out.println("Check your server connection or try back later.");
         }
-        in.close();
-        System.out.println(sb.toString());
     }
 }
