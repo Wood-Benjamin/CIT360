3f82e6381f9d8d9d897392194521536978e76b33
Wood-Benjamin <38670310+Wood-Benjamin@users.noreply.github.com>
2019-12-17 18:33:41 -0700

    updates

--- ThreadsER/src/Threads_ER/runnables.java
+++ ThreadsER/src/Threads_ER/runnables.java
@@ -3,7 +3,7 @@ package Threads_ER;
 import java.util.concurrent.atomic.AtomicInteger;
 //sited works: https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/atomic/AtomicInteger.html
 public class runnables implements Runnable {
-    private AtomicInteger count = new AtomicInteger(6);
+    private AtomicInteger count = new AtomicInteger(5);
     private int chash = new AtomicInteger(0).hashCode();
     //Returns a hash code value for the object. This method is supported for the benefit of hash tables such as those provided by HashMap.
     @Override
